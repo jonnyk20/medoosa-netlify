4472e8a7b6d22b8462f353cdfab8c94ae8192d80
Jonny Kalambay <jonnyk20@gmail.com>
2019-09-05 18:04:18 +0000

    0

--- src/redux/reducers/medoosa.js
+++ src/redux/reducers/medoosa.js
@@ -2,7 +2,7 @@ import { SET_MOD, SET_MODS, ADVANCE_STAGE } from "../actions/actionTypes"
 
 const initialState = {
   name: "Medoosa",
-  stage: 5,
+  stage: 0,
   modSelections: [
     {
       name: "color",
